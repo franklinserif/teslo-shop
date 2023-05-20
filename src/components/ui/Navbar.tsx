48207ad6f6d0e3e20fc914dc862d020873af80b9
Franklin Rodriguez <franklinserif@gmail.com>
2023-05-20 14:05:25 -0400

    feat: :lipstick: Navbar added

---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,64 @@
+import NextLink from "next/link";
+import {
+  AppBar,
+  Badge,
+  Box,
+  Button,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
+
+export const Navbar = () => {
+  return (
+    <AppBar>
+      <Toolbar>
+        <NextLink href="/" style={{ textDecoration: "none" }}>
+          <Link component="span" display="flex" alignItems="center">
+            <Typography variant="h6">Teslo |</Typography>
+            <Typography sx={{ ml: 0.5 }}>Shop</Typography>
+          </Link>
+        </NextLink>
+
+        <Box flex={1} />
+
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          <NextLink href="/category/men" style={{ textDecoration: "none" }}>
+            <Link component="span">
+              <Button>Men</Button>
+            </Link>
+          </NextLink>
+          <NextLink href="/category/women" style={{ textDecoration: "none" }}>
+            <Link component="span">
+              <Button>Women</Button>
+            </Link>
+          </NextLink>
+          <NextLink href="/category/kid" style={{ textDecoration: "none" }}>
+            <Link component="span">
+              <Button>Kid</Button>
+            </Link>
+          </NextLink>
+        </Box>
+
+        <Box flex={1} />
+
+        <IconButton>
+          <SearchOutlined />
+        </IconButton>
+        <NextLink href="/cart">
+          <Link>
+            <IconButton>
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCartOutlined />
+              </Badge>
+            </IconButton>
+          </Link>
+        </NextLink>
+
+        <Button>Menu</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
