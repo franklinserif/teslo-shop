3c603a3c17eca8c45d3706bd4bd2dbb77980cccf
Franklin Rodriguez <franklinserif@gmail.com>
2023-05-20 14:05:51 -0400

    feat: :lipstick: Add Layout to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { ShopLayout } from "@/components/layouts";
+import { Typography } from "@mui/material";
 
 export default function Home() {
   return (
-    <>
-      <h1>Teslo shop</h1>
-    </>
+    <ShopLayout
+      title="Teslo shop - Home"
+      pageDescription="Find the best product of Teslo here"
+      imageFullUrl={""}
+    >
+      <Typography variant="h1" component="h1">
+        Market
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        All products
+      </Typography>
+    </ShopLayout>
   );
 }
