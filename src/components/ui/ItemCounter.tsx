6dd1e2ed3f9659f32f8c23b3c6ed76ad9f267d1a
Franklin Rodriguez <franklinserif@gmail.com>
2023-05-21 22:19:44 -0400

    feat: :lipstick: Add ItemCounter component

---
+++ src/components/ui/ItemCounter.tsx
@@ -0,0 +1,19 @@
+import { Box, IconButton, Typography } from "@mui/material";
+import { RemoveCircleOutline, AddCircleOutline } from "@mui/icons-material";
+import { FC } from "react";
+
+interface Props {}
+
+export const ItemCounter: FC = () => {
+  return (
+    <Box display="flex" alignItems="center">
+      <IconButton>
+        <RemoveCircleOutline />
+      </IconButton>
+      <Typography sx={{ width: 40, textAlign: "center" }}>1</Typography>
+      <IconButton>
+        <AddCircleOutline />
+      </IconButton>
+    </Box>
+  );
+};
