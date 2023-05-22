e0c9419dcfafe8840a050c6d965c988aeae2bbe3
Franklin Rodriguez <franklinserif@gmail.com>
2023-05-21 22:15:04 -0400

    feat: :lipstick: Add ProductSlideShow component

---
+++ src/components/products/ProductSlideshow.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import { Slide } from "react-slideshow-image";
+import styles from "@/styles/product-slideshow.module.css";
+
+interface Props {
+  images: string[];
+}
+
+export const ProductSlideShow: FC<Props> = ({ images }) => {
+  return (
+    <Slide easing="ease" duration={7000} indicators>
+      {images.map((image) => {
+        const url = `/products/${image}`;
+        return (
+          <div key={image} className={styles["each-slide"]}>
+            <div
+              style={{
+                backgroundImage: `url(${url})`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </div>
+        );
+      })}
+    </Slide>
+  );
+};
