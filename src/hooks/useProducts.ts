d2bdb5b6da89e0a45cf708b1a05a88cc0be44c90
Franklin Rodriguez <franklinserif@gmail.com>
2023-05-25 00:22:00 -0400

    feat: :sparkles: Add useProducts customHook
    useProducts custom hook for data fetching with SWR

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,12 @@
+import { IProduct } from "@/interfaces";
+import useSWR, { SWRConfiguration } from "swr";
+
+export const useProducts = (url: string, config: SWRConfiguration = {}) => {
+  const { data, error } = useSWR<IProduct[]>(`/api${url}`, config);
+
+  return {
+    products: data || [],
+    isLoading: !data && !error,
+    isError: error,
+  };
+};
