fb3ff111ed4635ae620b123d58b82db9e42b03af
Franklin Rodriguez <franklinserif@gmail.com>
2023-05-25 00:21:24 -0400

    feat: :sparkles: Add SWRConfig provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,22 @@
-import "@/styles/globals.css";
+import { SWRConfig } from "swr";
 import { lightTheme } from "@/themes";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import type { AppProps } from "next/app";
 
+import "@/styles/globals.css";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) =>
+          fetch(resource, init).then((res) => res.json()),
+      }}
+    >
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
   );
 }
